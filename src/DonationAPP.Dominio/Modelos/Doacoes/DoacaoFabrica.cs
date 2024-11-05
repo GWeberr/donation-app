827b77d529fe52b1d6ef2d1091d247eb1c5fc7d4
Gabriel Weber da Silva <gabrielwsilva@hotmail.com>
2024-11-05 06:24:11 -0300

    Criada fabrica para o restante dos objetos de domínio

---
+++ src/DonationAPP.Dominio/Modelos/Doacoes/DoacaoFabrica.cs
@@ -0,0 +1,10 @@
+﻿namespace DonationAPP.Dominio.Modelos.Doacoes
+{
+    public static class DoacaoFabrica
+    {
+        public static Doacao Criar(Guid id, Guid instituicaoId, decimal valor)
+        {
+            return new Doacao(id, instituicaoId, valor);
+        }
+    }
+}
