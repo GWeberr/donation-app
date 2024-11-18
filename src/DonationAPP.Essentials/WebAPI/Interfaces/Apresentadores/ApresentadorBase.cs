a54298c13f231a96754c0a9eee108d08001b8808
Gabriel Weber da Silva <gabrielwsilva@hotmail.com>
2024-11-18 00:53:57 -0300

    Criado projeto essentials para concentrar as interfaces úteis

---
+++ src/DonationAPP.Essentials/WebAPI/Interfaces/Apresentadores/ApresentadorBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DonationAPP.Essentials.WebAPI.Interfaces.Apresentadores
+{
+    public abstract class ApresentadorBase
+    {
+        public IActionResult ViewModel { get; protected set; } = default!;
+    }
+}
